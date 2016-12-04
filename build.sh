6ecde6f0c8b517a85a05aec31d31bbdfdef852ae
Baker Bokorney <bbokorney@gmail.com>
2016-12-04 10:55:58 -0500

    Run the tests

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-set -e
+set -ex
+
+go test -v ./...
 
 hash=$(git rev-parse HEAD)
 version=$(git show-ref --tags -d | grep $hash | sed -e 's,.* refs/tags/,,' -e 's/\^{}//')
@@ -12,7 +14,6 @@ hash=$(echo $hash | cut -c 1-10)
 echo "Building version $version (commit $hash)"
 
 arch='amd64'
-  set -x
 for os in $(echo linux windows darwin); do
   GOOS=$os GOARCH=$arch go build -o build/shout-$os-$arch -ldflags "-X main.version=$version -X main.hash=$hash"
 done
