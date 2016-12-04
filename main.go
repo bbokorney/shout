9920a17ab47d7e36834d27fd036c3c976bca94b6
Baker Bokorney <bbokorney@gmail.com>
2016-12-04 09:34:52 -0500

    Add CLI

--- main.go
+++ main.go
@@ -1,25 +1,86 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/urfave/cli"
+)
+
+const (
+	defaultTemplateDir     = "templates"
+	defaultUsersConfigFile = "users.yml"
 )
 
 func main() {
-	log.Println("Starting up...")
+	app := cli.NewApp()
 
-	parsedTemplates, err := ParseTemplates("templates")
-	if err != nil {
-		log.Fatal(err)
+	app.Name = "shout"
+	app.Usage = "Be heard"
+	app.UsageText = "shout [options]"
+	app.Authors = []cli.Author{
+		cli.Author{
+			Name:  "Baker Bokorney",
+			Email: "bbokorney@gmail.com",
+		},
+	}
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:   "users, u",
+			Value:  "users.yml",
+			Usage:  "User mapping `FILE`",
+			EnvVar: "SHOUT_USERS_FILE",
+		},
+		cli.StringFlag{
+			Name:   "templates, t",
+			Value:  "templates",
+			Usage:  "Template directory `DIR`",
+			EnvVar: "SHOUT_TEMPLATES_DIR",
+		},
+		cli.StringFlag{
+			Name:   "slack-url, s",
+			Usage:  "Slack incoming webhook `URL`",
+			EnvVar: "SHOUT_SLACK_URL",
+		},
+		cli.IntFlag{
+			Name:   "port, p",
+			Usage:  "`PORT`",
+			Value:  8080,
+			EnvVar: "SHOUT_PORT",
+		},
 	}
 
-	users := NewUsers(make(map[string]string))
-	templates := NewTemplates(parsedTemplates)
-	notifications := NewNotifications("http://fake-url")
-	shouter := NewShouter(users, templates, notifications)
-	shoutHandler := NewShoutHandler(shouter)
+	app.Action = func(c *cli.Context) error {
 
-	http.HandleFunc("/shout", shoutHandler)
+		parsedTemplates, err := ParseTemplates(c.String("templates"))
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+		usersConfig, err := ReadUsersFile(c.String("users"))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		slackURL := c.String("slack-url")
+		if slackURL == "" {
+			log.Fatal("Slack url must be defined")
+		}
+
+		users := NewUsers(usersConfig)
+		templates := NewTemplates(parsedTemplates)
+		notifications := NewNotifications(slackURL)
+		shouter := NewShouter(users, templates, notifications)
+		shoutHandler := NewShoutHandler(shouter)
+
+		http.HandleFunc("/shout", shoutHandler)
+
+		log.Printf("Listening on port %d", c.Int("port"))
+		log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", c.Int("port")), nil))
+		return nil
+	}
+
+	app.Run(os.Args)
 }
